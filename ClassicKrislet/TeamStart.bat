a7e6be46a1d7e122238d25877c0e77af7d7b94bc
Unknown <abeerrafiq99@gmail.com>
2018-06-13 15:18:44 -0400

    "Port 7000" Removed from File

--- ClassicKrislet/TeamStart.bat
+++ ClassicKrislet/TeamStart.bat
@@ -1,4 +1,4 @@
-start java Krislet -team Carleton -port 7000
+start java Krislet -team Carleton
 ping localhost
-start java Krislet -team University -port 7000
+start java Krislet -team University
 ping localhost
