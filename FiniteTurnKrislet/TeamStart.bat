299ecfef94a1f89e19a17b31dcd816736e62849b
Patrick Gavigan <pgavigan@gmail.com>
2018-08-22 14:40:14 -0400

    Games now include 5 players per team

--- FiniteTurnKrislet/TeamStart.bat
+++ FiniteTurnKrislet/TeamStart.bat
@@ -1,3 +1,28 @@
+:: Player 1
+start java Krislet -team Carleton -port 7000
+ping localhost
+start java Krislet -team University -port 7000
+ping localhost
+
+:: Player 2
+start java Krislet -team Carleton -port 7000
+ping localhost
+start java Krislet -team University -port 7000
+ping localhost
+
+:: Player 3
+start java Krislet -team Carleton -port 7000
+ping localhost
+start java Krislet -team University -port 7000
+ping localhost
+
+:: Player 4
+start java Krislet -team Carleton -port 7000
+ping localhost
+start java Krislet -team University -port 7000
+ping localhost
+
+:: Player 5
 start java Krislet -team Carleton -port 7000
 ping localhost
 start java Krislet -team University -port 7000
